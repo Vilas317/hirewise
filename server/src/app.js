6bcaaa272789b1920d1586b88ea54912200efec5
Vilas <Vilashtamrakar@gmail.com>
2026-04-14 17:18:09 +0530

    add vercel cors

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,10 @@ const jobRoutes = require("./modules/job/job.routes");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://hirewise-nqlnngmf5-vilas-projects-3a847eb8.vercel.app"
+    ],
     credentials: true,
   })
 );
